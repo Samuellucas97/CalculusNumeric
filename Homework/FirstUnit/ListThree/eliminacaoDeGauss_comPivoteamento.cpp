2fa3d901fe33d97776876dc821e04a55e0832b69
Samuel Lucas de Moura Ferino <samuellucas97@A204-05L.imd.local>
2019-03-18 11:39:32 -0300

    adicionando leitura de matriz

--- Homework/FirstUnit/ListThree/eliminacaoDeGauss_comPivoteamento.cpp
+++ Homework/FirstUnit/ListThree/eliminacaoDeGauss_comPivoteamento.cpp
@@ -10,21 +10,30 @@ using std::cout;
 using std::endl;
 using std::cerr;
 
+#include <cstdlib>
+
 #include <fstream>
 using std::ifstream;
 
 #include <string>
 using std::string;
+using std::stoi;
+
+#include <vector>
+using std::vector;
 
 int main(int argc, char* argv[] ){
+
+    vector< vector<int> > matriz_A_b;
+    vector<int> linha_de_matriz_A_b;
 
     string nomeDoArquivo;
     string tamanhoMatriz_string;
     string conteudoDoArquivo;
-    int tamanhoMatriz;
 
     if( argc != 2 || ( atoi(argv[1]) != 1 && atoi(argv[1]) != 2 && atoi(argv[1]) != 3 ) ){
         cerr << "Voce so pode adicionar junto ao ./exe os seguintes argumento: 1, 2 ou 3..." << endl;
+        exit(1);
     }
 
     switch( atoi(argv[1]) ){
@@ -39,9 +48,9 @@ int main(int argc, char* argv[] ){
 
         default: nomeDoArquivo = "m3.in";
     }
-
+    
 	ifstream arqDados;
-	arqDados.open(nomeDoArquivo);
+	arqDados.open(nomeDoArquivo.c_str());
 
 	if(arqDados.is_open() == 0){ /// VERIFICANDO SE O ARQUIVO FOI ABERTO
 		cerr << "O arquivo nao foi aberto" << endl;
@@ -49,10 +58,27 @@ int main(int argc, char* argv[] ){
 	}
 
     arqDados >> tamanhoMatriz_string;
+    
+    for( int i = 0; i < stoi(tamanhoMatriz_string); ++i){
 
-    int A[tamanhoMatriz][tamanhoMatriz + 1];
+        for(int j = 0; j < stoi(tamanhoMatriz_string) + 1; ++j){    
+          arqDados >> conteudoDoArquivo;
+          linha_de_matriz_A_b.push_back( stoi(conteudoDoArquivo) );  
 
-    arqDados >> conteudoDoArquivo;
+        }    
+
+        matriz_A_b.push_back( linha_de_matriz_A_b);
+        linha_de_matriz_A_b.clear();
+
+    }  
+
+    for(auto& n: matriz_A_b){ /// IMPRIMINDO MATRIZ (A | b)
+        for(auto& j: n){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
 
 	arqDados.close();
 
