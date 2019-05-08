926bc3707686cfb13d83c807087b1155f29ceb28
Samuel Lucas <samuel1797@gmail.com>
2019-05-08 20:05:45 -0300

    Update metodosNumericos_em_IntegracaoNumerica.py

--- Homework/SecondUnit/ListFive/src/metodosNumericos_em_IntegracaoNumerica.py
+++ Homework/SecondUnit/ListFive/src/metodosNumericos_em_IntegracaoNumerica.py
@@ -60,11 +60,11 @@ print( "Aproximação obtida: "+ str(compositeTrapezoidMethod( lambda x: x*sin(x
 # In[40]:
 
 
-def methodSimpson1_3(function_f, a, b, numberOfIntervals = 6):
+def methodSimpson1_3(function_f, a, b, numberOfIntervals = 3):
     """
     Numerical integration using the Simpson 1/3 Rule.
 
-    integral = methodSimpson1_3(function_f, a, b, numberOfIntervals = 6)
+    integral = methodSimpson1_3(function_f, a, b, numberOfIntervals = 3)
 
     INPUT:
       * function_f: function to be integrated
@@ -78,20 +78,36 @@ def methodSimpson1_3(function_f, a, b, numberOfIntervals = 6):
     
     April 2019
     """
-    h = (b - a) / numberOfIntervals
-    return (h/3)*( function_f(a) + sum([ 2*function_f(a + i*h) if i%2 == 0 else 4*function_f(a + i*h) for i in range(1, numberOfIntervals) ]) + function_f(b) )
+    h = (b - a) / (2*numberOfIntervals)
+    return (h/3)*( function_f(a) + sum([ 2*function_f(a + (i*h)) if i%2 == 0 else 4*function_f(a + (i*h)) for i in range(1, numberOfIntervals*2) ]) + function_f(b) )
 
 print( "Aproximação obtida: "+ str(methodSimpson1_3( lambda x: x*sin(x), -5, 5 ) ) )
-
 
 # ## Método 3/8 de Simpson
 
 # In[70]:
 
 
-def methodSimpson3_8(function_f, a, b, numberOfIntervals = 6):
-    h = (b - a) / numberOfIntervals
-    return ((3*h)/8)*( function_f(a) + sum([ 2*function_f(a + i*h) if i%3 == 0 else 3*function_f(a + i*h) for i in range(1, numberOfIntervals) ]) + function_f(b) )
+def methodSimpson3_8(function_f, a, b, numberOfIntervals = 2):
+    """
+    Numerical integration using the Simpson 3/8 Rule.
+
+    integral = methodSimpson3_8(function_f, a, b, numberOfIntervals = 2)
+
+    INPUT:
+      * function_f: function to be integrated
+      * a: beginning of integration interval
+      * b: end of integration interval
+      * numberOfIntervals: number of interval divisions
+
+    return: \int_{a}^{b} f(x) dx
+
+    Author: Samuel Lucas
+    
+    May 2019
+    """
+    h = (b - a)  / (3*numberOfIntervals)
+    return ((3*h)/8)*( function_f(a) + sum([ 3*function_f(a + i*h) if i%3 != 0 else 2*function_f(a + i*h) for i in range(1, numberOfIntervals+1) ]) + function_f(b) )
 
 print( "Aproximação obtida: "+ str(methodSimpson3_8( lambda x: x*sin(x), -5, 5 ) ) )
 
