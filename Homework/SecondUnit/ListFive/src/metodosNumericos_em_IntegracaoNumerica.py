32f0c5fe90a47069729a621624a0496df28e9d8b
Samuel <samuel1797@gmail.com>
2019-05-08 23:50:42 -0300

    Atualizando repositorio com relatorios

--- Homework/SecondUnit/ListFive/src/metodosNumericos_em_IntegracaoNumerica.py
+++ Homework/SecondUnit/ListFive/src/metodosNumericos_em_IntegracaoNumerica.py
@@ -1,35 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Métodos numéricos em _Integração numérica_
-# 
-# ### Autor   
-# _Samuel Lucas de Moura Ferino_ ( samuellucas97@ufrn.edu.br )  
-# IMD/UFRN 2019.   
-
-# ## Introdução
-# 
-# 
-# Tendo em vista que queremos calcular $\int_{-5}^{5}  \! x sen(x) \, dx$ , utilizaremos os seguintes métodos numéricos:  
-# * [Método do trapézio](método-do-trapézio) 
-# * [Método 1/3 de Simpson](metodo-1/3-de-simpson)
-# * [Método 3/8 de Simpson](metodo-3/8-de-simpson)
-# 
-# *Obs.: O valor resultante do cálculo da respectiva integral definida é $-10cos(5) +2sen(5) \approx -4.75447 $.*
-
-# In[27]:
-
-
 from scipy.integrate import quad
 from numpy import sin
-
-print( "Confirmando que o resultado do cálculo dessa integral é " + str( quad( lambda x: x*sin(x), -5, 5 )[0] ) )
-
-
-# ## Método do trapézio
-
-# In[44]:
-
 
 def compositeTrapezoidMethod( function_f, a,  b, numberOfIntervals = 6):
     """
@@ -52,13 +22,9 @@ def compositeTrapezoidMethod( function_f, a,  b, numberOfIntervals = 6):
     h = (b - a) / numberOfIntervals 
     return (h/2)*( function_f(a) + sum([ 2*function_f(a + i*h) for i in range(1, numberOfIntervals) ]) + function_f(b) )
 
-print( "Aproximação obtida: "+ str(compositeTrapezoidMethod( lambda x: x*sin(x), -5, 5 ) ) )
-
-
 # ## Método 1/3 de Simpson
 
 # In[40]:
-
 
 def methodSimpson1_3(function_f, a, b, numberOfIntervals = 3):
     """
@@ -81,12 +47,9 @@ def methodSimpson1_3(function_f, a, b, numberOfIntervals = 3):
     h = (b - a) / (2*numberOfIntervals)
     return (h/3)*( function_f(a) + sum([ 2*function_f(a + (i*h)) if i%2 == 0 else 4*function_f(a + (i*h)) for i in range(1, numberOfIntervals*2) ]) + function_f(b) )
 
-print( "Aproximação obtida: "+ str(methodSimpson1_3( lambda x: x*sin(x), -5, 5 ) ) )
-
 # ## Método 3/8 de Simpson
 
 # In[70]:
-
 
 def methodSimpson3_8(function_f, a, b, numberOfIntervals = 2):
     """
@@ -107,7 +70,11 @@ def methodSimpson3_8(function_f, a, b, numberOfIntervals = 2):
     May 2019
     """
     h = (b - a)  / (3*numberOfIntervals)
-    return ((3*h)/8)*( function_f(a) + sum([ 3*function_f(a + i*h) if i%3 != 0 else 2*function_f(a + i*h) for i in range(1, numberOfIntervals+1) ]) + function_f(b) )
+    return ((3*h)/8)*( function_f(a) + sum([ 2*function_f(a + i*h) if i%3 == 0 else 3*function_f(a + i*h) for i in range(1, 3*numberOfIntervals - 1 ) ]) + function_f(b) )
 
-print( "Aproximação obtida: "+ str(methodSimpson3_8( lambda x: x*sin(x), -5, 5 ) ) )
+
+print( "Calculando-se analiticamente, temos que: " + str( quad( lambda x: x*sin(x), -5, 5 )[0] ) )
+
+for i in range(1,7):
+    print( "Aproximação obtida para i = " + str(i) + ": "+ str(compositeTrapezoidMethod( lambda x: x*sin(x), -5, 5, i*6 ) )  + ": "+ str(methodSimpson1_3( lambda x: x*sin(x), -5, 5, i*3 ) ) + ": "+ str(methodSimpson3_8( lambda x: x*sin(x), -5, 5, i*2 ) ) + "\n" )
 
